722ccc919d0e1e7da3bacc3c46f49d3eb02a4a04
Blake Cicenas <30779217+fornitsumfornis@users.noreply.github.com>
2018-05-06 18:59:39 -0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,18 @@
+#/bin/sh
+echo "Using 'www.timeanddate.com/sun/', find the city closest to and where you live."  
+read -p "Type name of the city:" city
+citydashed="$(echo $city | sed -e 's/ /-/g')"
+echo $citydashed
+sed -i "s/yourcity/$citydashed/g" sunrise-sunset.sh
+read -p "Input what you want your daytime brightness to be on a scale of 0 to 255:" day
+sed -i "s/wake/$day/g" sunrise-sunset.sh
+read -p "Input what you want your nighttime brightness to be on a scale of 0 to 255:" night
+sed -i "s/dim/$night/g" sunrise-sunset.sh
+echo "01 2   * * * root /bin/bash /usr/local/sbin/sunrise-sunset.sh"> picron.txt 
+sudo cat picron.txt>>/etc/crontab 
+sudo cp sunrise-sunset.sh /usr/local/sbin
+rm picron.txt
+sudo chown root.pi /sys/class/backlight/rpi_backlight/brightness
+sudo chmod 0644 /sys/class/backlight/rpi_backlgiht/brightness
+
+
---
+++ sunrise-sunset.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#Pulling information from website www.timeanddate.com, just place the website 
+#for your location
+
+#Delete the old information
+sed -i '$ d' /etc/crontab
+
+#Grab sunrise and sunset information 
+lynx --dump https://www.timeanddate.com/sun/usa/yourcity > daylight.txt
+cat daylight.txt | sed -n -e 's/^.*Sunrise Today: //p'> sunrise.txt
+cat daylight.txt | sed -n -e 's/^.*Sunset Today: //p' > sunset.txt
+
+#sunrise
+echo -n "0" >rise.txt
+cat sunrise.txt | cut -c1,3,4 -n >>rise.txt
+cat rise.txt | sed 's/.\{2\}/& /g'>day.txt
+sed -i 's/\([a-zA-Z0-9][a-zA-Z0-9]*\) \([a-zA-Z0-9][a-zA-Z0-9]*\)/\2 \1/' day.txt
+sudo chmod 777 day.txt
+echo -n "$(cat day.txt)" >day.txt 
+
+#Input the daytime brightness below
+echo -n " * * * root echo wake > /sys/class/backlight/rpi_backlight/brightness" >>day.txt
+sed ' ' day.txt >>/etc/crontab
+
+#sunset
+
+cat sunset.txt | cut -c1,3,4 -n > set.txt
+declare -i num
+declare -i night
+num=( $(<set.txt) )
+declare -i base=$((1200))
+sum=$((num + base))
+echo $sum>night.txt 
+cat night.txt | sed 's/.\{2\}/& /g'>night2.txt
+sed -i 's/\([a-zA-Z0-9][a-zA-Z0-9]*\) \([a-zA-Z0-9][a-zA-Z0-9]*\)/\2 \1/' night2.txt
+sudo chmod 777 night2.txt
+echo -n "$(cat night2.txt)" >night2.txt
+
+#Input the Nighttime Brightness Below
+echo -e " * * * root echo dim > /sys/class/backlight/rpi_backlight/brightness\n" >>night2.txt
+sed 'i\ ' night2.txt >> /etc/crontab
+
+#deleting
+sudo rm /usr/local/sbin/sunrise.txt /usr/local/sbin/sunset.txt /usr/local/sbin/rise.txt /usr/local/sbin/day.txt /usr/local/sbin/set.txt /usr/local/sbin/night.txt /usr/local/sbin/night2.txt /usr/local/sbin/daylight.txt 
