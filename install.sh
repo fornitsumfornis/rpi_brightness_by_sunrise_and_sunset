329cf74167e690962310e31b7ab9c35231ee43ec
Blake Cicenas <30779217+fornitsumfornis@users.noreply.github.com>
2018-05-07 13:32:22 -0500

    Took out unimportant echo and added documentation

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 echo "Using 'www.timeanddate.com/sun/', find the city closest to and where you live."  
 read -p "Type name of the city:" city
 citydashed="$(echo $city | sed -e 's/ /-/g')"
-echo $citydashed
 sed -i "s/yourcity/$citydashed/g" sunrise-sunset.sh
 read -p "Input what you want your daytime brightness to be on a scale of 0 to 255:" day
 sed -i "s/wake/$day/g" sunrise-sunset.sh
@@ -12,6 +11,8 @@ echo "01 2   * * * root /bin/bash /usr/local/sbin/sunrise-sunset.sh"> picron.txt
 sudo cat picron.txt>>/etc/crontab 
 sudo cp sunrise-sunset.sh /usr/local/sbin
 rm picron.txt
+
+#Un-setting Restrictions so that you will be allowed to change the backlight of your pi  
 sudo chown root.pi /sys/class/backlight/rpi_backlight/brightness
 sudo chmod 0644 /sys/class/backlight/rpi_backlgiht/brightness
 
