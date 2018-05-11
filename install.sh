035ed27596fde9ea47f4bfc9e52b0be3bf283362
Blake Cicenas <30779217+fornitsumfornis@users.noreply.github.com>
2018-05-11 09:40:14 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,13 +7,14 @@ read -p "Input what you want your daytime brightness to be on a scale of 0 to 25
 sed -i "s/wake/$day/g" sunrise-sunset.sh
 read -p "Input what you want your nighttime brightness to be on a scale of 0 to 255:" night
 sed -i "s/dim/$night/g" sunrise-sunset.sh
+pi=$(whoami)
 echo "01 2   * * * root /bin/bash /usr/local/sbin/sunrise-sunset.sh"> picron.txt 
 sudo cat picron.txt>>/etc/crontab 
 sudo cp sunrise-sunset.sh /usr/local/sbin
 rm picron.txt
 
 #Un-setting Restrictions so that you will be allowed to change the backlight of your pi  
-sudo chown root.pi /sys/class/backlight/rpi_backlight/brightness
+sudo chown root.$pi /sys/class/backlight/rpi_backlight/brightness
 sudo chmod 0644 /sys/class/backlight/rpi_backlgiht/brightness
 
 
